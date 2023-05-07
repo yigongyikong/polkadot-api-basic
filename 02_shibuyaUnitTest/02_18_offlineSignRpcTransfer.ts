5d939a11c7e8dbb8f3b523115b91dde6312643cb
yigongyikong <yigongyikong@gmail.com>
2023-05-07 06:19:10 +0000

    20230507 metadata 02_18_example

--- 02_shibuyaUnitTest/02_18_offlineSignRpcTransfer.ts
+++ 02_shibuyaUnitTest/02_18_offlineSignRpcTransfer.ts
@@ -20,6 +20,7 @@ import { EXTRINSIC_VERSION } from '@polkadot/types/extrinsic/v4/Extrinsic';
 const offlineSignRpcTransfer = async () => {
     // Create a new instance of the api
     const provider = new WsProvider('wss://rpc.shibuya.astar.network');
+    // const provider = new WsProvider('wss://hazel-gondolin-982d6.astar.bdnodes.net/para-websocket?auth=KpsskkIce4d-ogsKq42cUirhP29tqwW3bOH4sfpPuJ8');
     const api = new ApiPromise({ provider });
 
     const testAcnt1 = 'VxJhKGJ7wsVwZvaikWQzhexb9VawgFUtBuXNAmDAhBBVi4U';
@@ -30,21 +31,27 @@ const offlineSignRpcTransfer = async () => {
     const keyring = new Keyring({ type: 'sr25519' });
     const testAcnt2Mnemonic = 'slice bronze until cabin gold cradle foil carbon measure owner topple dignity';
     const testAcnt2Key = keyring.addFromUri(testAcnt2Mnemonic);
-    console.log(
-        "testAcnt2Key's SS58-Encoded Address:",
-        deriveAddress(testAcnt2Key.publicKey, 42) // TODO, use correct prefix
-    );
+    // console.log(
+    //     "testAcnt2Key's SS58-Encoded Address:",
+    //     deriveAddress(testAcnt2Key.publicKey, 42) // TODO, use correct prefix
+    // );
     // testAcnt2Key's SS58-Encoded Address: 5HBSNV13JH7LuEtJUBNuKgcaPgNaXGBZhCknUWrKmD9p8vhA
 
     const { block } = await rpcToLocalNode('chain_getBlock');
     const blockHash = await rpcToLocalNode('chain_getBlockHash');
     const genesisHash = await rpcToLocalNode('chain_getBlockHash', [0]);
     const metadataRpc = await rpcToLocalNode('state_getMetadata');
+    // console.log(metadataRpc)
+    const metadataRpc_api = await api.rpc.state.getMetadata();
+    // console.log(metadataRpc_api);
+    // console.log(metadataRpc_api.toHex());
 
     const { specVersion, transactionVersion, specName } = await rpcToLocalNode(
         'state_getRuntimeVersion'
     );
-    
+    console.log(specVersion)
+    const specVersion_api = await api.runtimeVersion.specVersion.toString();
+    console.log(specVersion_api)
     const registry = getRegistry({
         chainName: 'shibuya',
         specName,
@@ -54,7 +61,7 @@ const offlineSignRpcTransfer = async () => {
 
     const accountBalInfo = await api.derive.balances.account(testAcnt2);
     const nonceTestAcnt2 = accountBalInfo.accountNonce.toString();
-    console.log(`nonceTestAcnt2: ${nonceTestAcnt2}`); // nonceTestAcnt2: 19
+    // console.log(`nonceTestAcnt2: ${nonceTestAcnt2}`); // nonceTestAcnt2: 19
 
     const unsigned = methods.balances.transfer(
         {
@@ -88,16 +95,16 @@ const offlineSignRpcTransfer = async () => {
         metadataRpc,
         registry,
     });
-    console.log(
-        // TODO all the log messages need to be updated to be relevant to the method used
-        `\nDecoded Transaction\n  To: ${decodedUnsigned.method.args.dest}\n` + // To: [object Object]
-        `  Amount: ${decodedUnsigned.method.args.value}` // Amount: 20000000000000
-    );
+    // console.log(
+    //     // TODO all the log messages need to be updated to be relevant to the method used
+    //     `\nDecoded Transaction\n  To: ${decodedUnsigned.method.args.dest}\n` + // To: [object Object]
+    //     `  Amount: ${decodedUnsigned.method.args.value}` // Amount: 20000000000000
+    // );
 
 
     // Construct the signing payload from an unsigned transaction.
     const signingPayload = construct.signingPayload(unsigned, { registry });
-    console.log(`\nPayload to Sign: ${signingPayload}`); 
+    // console.log(`\nPayload to Sign: ${signingPayload}`); 
     // Payload to Sign: 0xa81f000000c499d3bbe996dfbb5549064d1890def4a9775a675b43b9203eefd810b21f650b0040e59c301215024c005c00000002000000ddb89973361a170839f80f152d2e9e38a376a5a7eccefcade763f46a8e56701912010dfa3428db09af032a13e49346b54ca75f3b955a2b4f64262ed780e62103
 
     // Decode the information from a signing payload.
@@ -105,12 +112,13 @@ const offlineSignRpcTransfer = async () => {
         metadataRpc,
         registry,
     });
-    console.log(
-        // TODO all the log messages need to be updated to be relevant to the method used
-        `\nDecoded Transaction\n  To: ${payloadInfo.method.args.dest}\n` + // To: [object Object]
-        `  Amount: ${payloadInfo.method.args.value}` // Amount: 20000000000000
-    );
+    // console.log(
+    //     // TODO all the log messages need to be updated to be relevant to the method used
+    //     `\nDecoded Transaction\n  To: ${payloadInfo.method.args.dest}\n` + // To: [object Object]
+    //     `  Amount: ${payloadInfo.method.args.value}` // Amount: 20000000000000
+    // );
 
+    /*
     // Sign a payload. This operation should be performed on an offline device.
     // const signature = signWith(alice, signingPayload, {
     const signature = signWith(testAcnt2Key, signingPayload, {
@@ -151,6 +159,7 @@ const offlineSignRpcTransfer = async () => {
     //     `\nDecoded Transaction\n  To: ${txInfo.method.args.dest}\n` + // 
     //     `  Amount: ${txInfo.method.args.value}\n`
     // );
+    */
 }
 
 offlineSignRpcTransfer().catch(console.error).finally(() => process.exit());
@@ -160,7 +169,9 @@ export function rpcToLocalNode(
     method: string,
     params: any[] = []
 ): Promise<any> {
-    return fetch('https://shibuya.public.blastapi.io', {
+    // return fetch('https://evm.shibuya.astar.network', {
+    // return fetch('https://shibuya.public.blastapi.io', { // origin refer
+    return fetch('https://hazel-gondolin-982d6.astar.bdnodes.net/para-http-rpc?auth=KpsskkIce4d-ogsKq42cUirhP29tqwW3bOH4sfpPuJ8', {
         body: JSON.stringify({
             id: 1,
             jsonrpc: '2.0',
